6894dd1839a63544e07c920227830bb4af40a405
mazyu36 <yiyth.fcb6@gmail.com>
2022-09-11 15:07:25 +0900

    Initial commit

---
+++ test/cdk_s3_event.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkS3Event from '../lib/cdk_s3_event-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_s3_event-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkS3Event.CdkS3EventStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
